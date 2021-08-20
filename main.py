da9c0a242865381362515a361ae3ef4ee4c70192
Juanma Fernandez <juanmanuelf12@gmail.com>
2021-08-20 15:51:19 -0300

    graph initial implementation

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
 from linked_list import LinkedList
+from graph import Graph
 
-l = LinkedList()
-l.add(1)
-l.add(3)
-l.add(6)
-l.add(2)
-l.printList()
-print(l.dequeue())
-l.printList()
-print(l.dequeue())
-l.printList()
+g = Graph(5)
+g.printGraph()

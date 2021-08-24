11a468ae53694830505d10b7a6beb593e080b83c
Juan Manuel <juanmanuelf12@gmail.com>
2021-08-24 13:37:35 -0300

    updated tree structure

--- tree.py
+++ tree.py
@@ -1,3 +1,4 @@
+#Estructura utilizada para formar el arbol de expansión de los caminos
 class Node:
     parent = None
     children = []
@@ -10,6 +11,16 @@ class Node:
     def addChild(self,value):
         newNode = Node(value,self)
         self.children.append(newNode)
+        newNode.parent = self
+
+    #Devuelve el camino hasta la raiz a partir de un nodo
+    def pathToRoot(self):
+        currNode = self
+        path = []
+        while currNode != None:
+            path.append(currNode.value)
+            currNode = currNode.parent
+        return path
 
 class Tree:
     root = None
@@ -17,3 +28,5 @@ class Tree:
     def __init__(self,value): 
         newNode = Node(value)
         self.root = newNode
+
+    
